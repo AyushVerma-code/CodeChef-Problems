4f5897a00a761a6f40b13f6802b84c6717406c39
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-05-13 15:59:01 +0530

    Create VALTRY.java

---
+++ VALTRY.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int busnum= sc.nextInt();
+		
+		if(busnum%5==0 || busnum%6==0)
+		{
+		    System.out.println("YES");
+		    
+		     
+		}
+		else 
+		{
+		    System.out.println("NO");
+		}
+	
+	}
+}
