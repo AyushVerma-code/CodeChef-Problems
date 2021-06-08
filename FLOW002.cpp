2ad70e77e2ca0d99804b5690e4b3c43fb0e9b589
Ayush Verma <78836695+AyushVerma-code@users.noreply.github.com>
2021-06-08 09:07:33 +0530

    Create FLOW002.cpp

---
+++ FLOW002.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int a,b,rem;
+        cin>>a>>b;
+        rem=a%b;
+        cout<<rem<<endl;
+        
+    }
+}
